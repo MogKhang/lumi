c8cf54bcdba0edf2db01d744de3085ea4776f8ec
MogKhang <khangnmit1@gmail.com>
2026-06-13 02:54:27 +0700

    chore: downgrade project version, clean up package references, and add macOS build script for DMG generation

---
+++ build_macos.sh
@@ -0,0 +1,13 @@
+# Move to the build folder
+cd build/macos/Build/Products/Release/
+
+# Generate the DMG file
+create-dmg \
+  --volname "Lumi Installer" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "Lumi.app" 175 120 \
+  --app-drop-link 425 120 \
+  "Lumi.dmg" \
+  "Lumi.app"
